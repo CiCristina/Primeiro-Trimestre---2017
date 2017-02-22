2cec99b30da017354dc50f11ff473bdbbf6b9701
Aluno <aluno@udicentro.local>
2017-02-22 10:28:07 -0300

    Programando o comportamento da lava

--- Assets/Scripts/OscilaZ.cs
+++ Assets/Scripts/OscilaZ.cs
@@ -1,16 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class OscilaZ : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float velocidade;
+    public float min;
+    public float max;
+    public float espera;
+
+
+    void Start() {
+        StartCoroutine(Move(max));
+
+    }
+
+    IEnumerator Move(float destino)
+    {
+        while (Mathf.Abs(destino - transform.localPosition.z) > 0.2f) {
+            Vector3 direcao = (destino == max) ? Vector3.forward : Vector3.back;
+            Vector3 velocidadeVetorial = direcao * velocidade;
+            transform.localPosition = transform.localPosition + velocidadeVetorial * Time.deltaTime;
+            yield return null;
+}
+        yield return new WaitForSeconds(espera);
+
+        destino = (destino == max) ? min : max;
+        StartCoroutine(Move(destino));
+
+   
+    }
 }
