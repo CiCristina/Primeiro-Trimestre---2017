4e3cd3970e722fe73adbfdf3ce3a2a6e189c31f9
Cibelly Cristina <cibellycristinaudi@gmail.com>
2017-03-07 11:03:03 -0300

    Incluindo o inimigo Gosma

---
+++ Assets/Scripts/FantasmaMove.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FantasmaMove : MonoBehaviour {
+
+    public float velocidade;
+    public float min;
+    public float max;
+    public float espera;
+
+
+    public float velocidade2 = 1f;
+    public float limite;
+    public float retorno;
+
+
+
+    void Start()
+    {
+        StartCoroutine(Move(max));
+
+    }
+
+    IEnumerator Move(float destino)
+    {
+        while (Mathf.Abs(destino - transform.localPosition.y) > 0.2f)
+        {
+            Vector3 direcao = (destino == max) ? Vector3.up : Vector3.down;
+            Vector3 velocidadeVetorial = direcao * velocidade;
+            transform.localPosition = transform.localPosition + velocidadeVetorial * Time.deltaTime;
+            yield return null;
+        }
+        yield return new WaitForSeconds(espera);
+
+        destino = (destino == max) ? min : max;
+        StartCoroutine(Move(destino));
+
+
+    }
+
+    void Update()
+    {
+
+
+        Vector3 velocidadeVetorial1 = Vector3.left * velocidade2;
+
+        transform.position = transform.position + velocidadeVetorial1 * Time.deltaTime;
+
+        if (transform.position.x <= limite)
+        {
+            transform.position = new Vector3(retorno, transform.position.y, transform.position.z);
+        }
+
+    }
+}
+
+
+
+
+   
